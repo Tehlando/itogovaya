be94cde5955ee146ac3a695620a728a131de2476
Tehlando <tehland2907@mail.ru>
2022-12-07 16:09:12 +0700

    Написана программа

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] array = {"калыван", "1245", "zimbabwe", "12", "aes"};
+Console.WriteLine (String.Join(' ', array));
+Console.WriteLine();
+string variable = string.Empty;
+for ( int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <4) variable = variable +array[i]+ " ";
+}
+
+string[] result = variable.Split(' ');
+Console.WriteLine(String.Join(' ',result));
